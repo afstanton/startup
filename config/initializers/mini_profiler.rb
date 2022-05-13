6cbc65c22da9e6f964d5dc4b927cdaf7f7a57416
Aaron F Stanton <afstanton@gmail.com>
2022-05-13 15:59:11 -0400

    Rubocop

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -2,7 +2,7 @@
 
 if Rails.env.development?
   # set RedisStore
-  Rack::MiniProfiler.config.storage_options = { url: ENV['REDIS_URL'] }
+  Rack::MiniProfiler.config.storage_options = { url: ENV.fetch('REDIS_URL', nil) }
   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 
   # For Hotwire
